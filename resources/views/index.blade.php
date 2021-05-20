a3e981b78cf1d7d9222425568f3907eb5b78f85a
Reedmaster <arspare22496@gmail.com>
2021-05-20 15:31:58 +0100

    updated index.blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,264 @@
 @extends('layouts.app')
 
 @section('content')
-    index here
+    <div class="container mx-auto px-4">
+        <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Popular Games</h2>
+        <div class="popular-games text-sm grid grid-cols-6 gap-12 border-b border-gray-800 pb-16">
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="#">
+                        <img src="" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px; bottom: -20px">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">
+                    Video game
+                </a>
+                <div class="text-gray-400 mt-1">PC</div>
+            </div>
+
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="#">
+                        <img src="" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px; bottom: -20px">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">
+                    Video game
+                </a>
+                <div class="text-gray-400 mt-1">PC</div>
+            </div>
+
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="#">
+                        <img src="" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px; bottom: -20px">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">
+                    Video game
+                </a>
+                <div class="text-gray-400 mt-1">PC</div>
+            </div>
+
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="#">
+                        <img src="" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px; bottom: -20px">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">
+                    Video game
+                </a>
+                <div class="text-gray-400 mt-1">PC</div>
+            </div>
+
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="#">
+                        <img src="" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px; bottom: -20px">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">
+                    Video game
+                </a>
+                <div class="text-gray-400 mt-1">PC</div>
+            </div>
+
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="#">
+                        <img src="" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px; bottom: -20px">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">
+                    Video game
+                </a>
+                <div class="text-gray-400 mt-1">PC</div>
+            </div>
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="#">
+                        <img src="" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px; bottom: -20px">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">
+                    Video game
+                </a>
+                <div class="text-gray-400 mt-1">PC</div>
+            </div>
+            
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="#">
+                        <img src="" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full" style="right: -20px; bottom: -20px">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">
+                    Video game
+                </a>
+                <div class="text-gray-400 mt-1">PC</div>
+            </div>
+        </div> {{-- end popular games --}}
+
+        <div class="flex my-10">
+            <div class="recently-reviewed w-3/4 mr-32">
+                <h2 class="text-blue-500 uppercase tracking-wide font-semibold">
+                    Recently Reviewed
+                </h2>
+                <div class="recently-reviewed-container space-y-12 mt-8">
+                    <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
+                        <div class="relative flex-none">
+                            <a href="#">
+                                <img src="" alt="game cover" class="w-20 lg:w-48 hover:opacity-75 transition ease-in-out duration-150">
+                            </a>
+                            <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full" 
+                                style="right: -20px; bottom: -20px"
+                            >
+                                <div class="font-semibold text-xs flex justify-center items-center h-full">
+                                    80%
+                                </div>
+                            </div>
+                        </div>
+                        <div class="ml-12">
+                            <a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-8">
+                                Video game
+                            </a>
+                            <div class="text-gray-400 mt-1">PC</div>
+                            <p class="mt-6 text-gray-400">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim blandit volutpat maecenas volutpat blandit aliquam etiam erat.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
+                        <div class="relative flex-none">
+                            <a href="#">
+                                <img src="" alt="game cover" class="w-20 lg:w-48 hover:opacity-75 transition ease-in-out duration-150">
+                            </a>
+                            <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full" 
+                                style="right: -20px; bottom: -20px"
+                            >
+                                <div class="font-semibold text-xs flex justify-center items-center h-full">
+                                    80%
+                                </div>
+                            </div>
+                        </div>
+                        <div class="ml-12">
+                            <a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-8">
+                                Video game
+                            </a>
+                            <div class="text-gray-400 mt-1">PC</div>
+                            <p class="mt-6 text-gray-400">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim blandit volutpat maecenas volutpat blandit aliquam etiam erat.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
+                        <div class="relative flex-none">
+                            <a href="#">
+                                <img src="" alt="game cover" class="w-20 lg:w-48 hover:opacity-75 transition ease-in-out duration-150">
+                            </a>
+                            <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full" 
+                                style="right: -20px; bottom: -20px"
+                            >
+                                <div class="font-semibold text-xs flex justify-center items-center h-full">
+                                    80%
+                                </div>
+                            </div>
+                        </div>
+                        <div class="ml-12">
+                            <a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-8">
+                                Video game
+                            </a>
+                            <div class="text-gray-400 mt-1">PC</div>
+                            <p class="mt-6 text-gray-400">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim blandit volutpat maecenas volutpat blandit aliquam etiam erat.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div> {{-- end recently reviewed --}}
+
+            <div class="most-anticipated lg:w-1/4 mt-12 lg:mt-0">
+                <h2 class="text-blue-500 uppercase tracking-wide font-semibold">
+                    Most Anticipated
+                </h2>
+                <div class="most-anticipated-container space-y-10 mt-8">
+                    <div class="game flex">
+                        <a href="#">
+                            <img src="" alt="game cover" class="w-16 hover:opactiy-75 transition ease-in-out duration-150">
+                        </a>
+                        <div class="ml-4">
+                            <a href="" class="hover:text-gray-300">Video game</a>
+                            <div class="text-gray-400 text-sm mt-1">Sept 16, 2021</div>
+                        </div>
+                    </div>
+                    <div class="game flex">
+                        <a href="#">
+                            <img src="" alt="game cover" class="w-16 hover:opactiy-75 transition ease-in-out duration-150">
+                        </a>
+                        <div class="ml-4">
+                            <a href="" class="hover:text-gray-300">Video game</a>
+                            <div class="text-gray-400 text-sm mt-1">Sept 16, 2021</div>
+                        </div>
+                    </div>
+                    <div class="game flex">
+                        <a href="#">
+                            <img src="" alt="game cover" class="w-16 hover:opactiy-75 transition ease-in-out duration-150">
+                        </a>
+                        <div class="ml-4">
+                            <a href="" class="hover:text-gray-300">Video game</a>
+                            <div class="text-gray-400 text-sm mt-1">Sept 16, 2021</div>
+                        </div>
+                    </div>
+                    <div class="game flex">
+                        <a href="#">
+                            <img src="" alt="game cover" class="w-16 hover:opactiy-75 transition ease-in-out duration-150">
+                        </a>
+                        <div class="ml-4">
+                            <a href="" class="hover:text-gray-300">Video game</a>
+                            <div class="text-gray-400 text-sm mt-1">Sept 16, 2021</div>
+                        </div>
+                    </div>
+                </div>
+            </div> {{-- end most anticipated --}}
+
+            <h2 class="text-blue-500 uppercase tracking-wide font-semibold mt-12">Coming Soon</h2>
+            <div class="most-anticipated-container space-y-10 mt-8">
+                <div class="game flex">
+                    <a href="#">
+                        <img src="" alt="game cover" class="w-16 hover:opactiy-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="ml-4">
+                        <a href="" class="hover:text-gray-300">Video game</a>
+                        <div class="text-gray-400 text-sm mt-1">Sept 16, 2021</div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>{{-- end container --}}
 @endsection
