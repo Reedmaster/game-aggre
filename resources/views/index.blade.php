80031051d6d5578b42b6b3d8430bfb496af078d8
Reedmaster <arspare22496@gmail.com>
2021-05-21 13:41:51 +0100

    made mobile responsive

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container mx-auto px-4">
         <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Popular Games</h2>
-        <div class="popular-games text-sm grid grid-cols-6 gap-12 border-b border-gray-800 pb-16">
+        <div class="popular-games text-sm grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 xl:grid-cols-6 gap-12 border-b border-gray-800 pb-16">
             <div class="game mt-8">
                 <div class="relative inline-block">
                     <a href="#">
@@ -124,16 +124,17 @@
             </div>
         </div> {{-- end popular games --}}
 
-        <div class="flex my-10">
-            <div class="recently-reviewed w-3/4 mr-32">
-                <h2 class="text-blue-500 uppercase tracking-wide font-semibold">
-                    Recently Reviewed
-                </h2>
+        <div class="flex flex-col lg:flex-row my-10">
+            <div class="recently-reviewed w-full lg:w-3/4 mr-0 lg:mr-32">
+                <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Recently Reviewed</h2>
                 <div class="recently-reviewed-container space-y-12 mt-8">
                     <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
                         <div class="relative flex-none">
                             <a href="#">
-                                <img src="" alt="game cover" class="w-20 lg:w-48 hover:opacity-75 transition ease-in-out duration-150">
+                                <img src="" 
+                                    alt="game cover" 
+                                    class="w-20 lg:w-48 hover:opacity-75 transition ease-in-out duration-150"
+                                >
                             </a>
                             <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full" 
                                 style="right: -20px; bottom: -20px"
@@ -148,7 +149,7 @@
                                 Video game
                             </a>
                             <div class="text-gray-400 mt-1">PC</div>
-                            <p class="mt-6 text-gray-400">
+                            <p class="mt-6 text-gray-400 hidden lg:block">
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim blandit volutpat maecenas volutpat blandit aliquam etiam erat.
                             </p>
                         </div>
@@ -171,7 +172,7 @@
                                 Video game
                             </a>
                             <div class="text-gray-400 mt-1">PC</div>
-                            <p class="mt-6 text-gray-400">
+                            <p class="mt-6 text-gray-400 hidden lg:block">
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim blandit volutpat maecenas volutpat blandit aliquam etiam erat.
                             </p>
                         </div>
@@ -194,7 +195,7 @@
                                 Video game
                             </a>
                             <div class="text-gray-400 mt-1">PC</div>
-                            <p class="mt-6 text-gray-400">
+                            <p class="mt-6 text-gray-400 hidden lg:block">
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim blandit volutpat maecenas volutpat blandit aliquam etiam erat.
                             </p>
                         </div>
