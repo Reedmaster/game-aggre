3b8591521c5ef8590f643b6eac9834b19747767a
Reedmaster <arspare22496@gmail.com>
2021-06-10 10:27:56 +0100

    refactor views logic to livewire

--- app/Http/Livewire/PopularGames.php
+++ app/Http/Livewire/PopularGames.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class PopularGames extends Component
@@ -18,7 +19,7 @@ class PopularGames extends Component
         $after = Carbon::now()->addMonths(2)->timestamp;
 
         // Cache data so it's retrieved quickly for subsequent requests
-        $this->popularGames = Cache::remember('popular-games', 7, function () use ($before, $after) {
+        $popularGamesUnformatted = Cache::remember('popular-games', 7, function () use ($before, $after) {
             // Http facade is based around guzzle, allows for easy HTTP requests
             return Http::withHeaders([
                 'Client-ID' => env('IGDB_CLIENT_ID'),
@@ -35,10 +36,24 @@ class PopularGames extends Component
                 )->post('https://api.igdb.com/v4/games')
                 ->json();
         });
+
+        // Call the formatForView
+        $this->popularGames = $this->formatForView($popularGamesUnformatted);
     }
 
     public function render()
     {
         return view('livewire.popular-games');
+    }
+
+    // Refactors the front end logic to here to make views lighter, plus separation of concerns
+    public function formatForView($games)
+    {
+        return collect($games)->map(function ($game) {
+            return collect($game)->merge([
+                // Place front end string here
+                'coverImageUrl' => Str::replaceFirst('thumb', 'cover_big', $game['cover']['url']),
+            ]);
+        })->toArray();
     }
 }
