c0f37fc63fded8f881787bc04f85b5c4cff06c2d
Reedmaster <arspare22496@gmail.com>
2021-06-11 13:56:48 +0100

    refactored show blade to livewire

--- app/Http/Controllers/GamesController.php
+++ app/Http/Controllers/GamesController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 
 class GamesController extends Controller
 {
@@ -62,13 +62,56 @@ class GamesController extends Controller
             )->post('https://api.igdb.com/v4/games')
             ->json();
 
-            dump($game);
-
         abort_if(!$game, 404);
 
         return view('show', [
-            'game' => $game[0],
+            'game' => $this->formatGameForView($game[0]),
         ]);
+    }
+
+    private function formatGameForView($game)
+    {
+        $temp = collect($game)->merge([
+            'coverImageUrl' => Str::replaceFirst('thumb', 'cover_big', $game['cover']['url']),
+            // Notice @foreach on blade is using collect here in livewire
+            'genres' => collect($game['genres'])->pluck('name')->filter()->implode(', '),
+            'involvedCompanies' => $game['involved_companies'][0]['company']['name'],
+            'platforms' => collect($game['platforms'])->pluck('abbreviation')->filter()->implode(', '),
+            'memberRating' => isset($game['rating']) ? round($game['rating']) . '%' : '0%',
+            'criticRating' => isset($game['aggregated_rating']) ? round($game['aggregated_rating']) . '%' : '0%',
+            'trailer' => 'https://youtube.com/watch/' . $game['videos'][0]['video_id'],
+            'screenshots' => collect($game['screenshots'])->map(function ($screenshot) {
+                return [
+                    'huge' => Str::replaceFirst('thumb', 'screenshot_huge', $screenshot['url']),
+                    'big' => Str::replaceFirst('thumb', 'screenshot_big', $screenshot['url']),
+                ];
+            })->take(9),
+            'similarGames' => collect($game['similar_games'])->map(function ($game) {
+                return collect($game)->merge([
+                    'coverImageUrl' => Str::replaceFirst('thumb', 'cover_big', $game['cover']['url']),
+                    'total_rating' => isset($game['total_rating']) ? round($game['total_rating']) . '%' : null,
+                    'platforms' => collect($game['platforms'])->pluck('abbreviation')->filter()->implode(', '),
+                ]);
+            })->take(5),
+            'social' => [
+                'official' => collect($game['websites'])->filter(function ($website) {
+                    return $website['category'] == 1;
+                })->first(),
+                'facebook' => collect($game['websites'])->filter(function ($website) {
+                    return $website['category'] == 4;
+                })->first(),
+                'twitter' => collect($game['websites'])->filter(function ($website) {
+                    return $website['category'] == 5;
+                })->first(),
+                'instagram' => collect($game['websites'])->filter(function ($website) {
+                    return $website['category'] == 8;
+                })->first(),
+            ],
+        ]);
+
+        dump($temp);
+        
+        return $temp;
     }
 
     /**
